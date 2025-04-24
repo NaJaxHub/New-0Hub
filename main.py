f58097c8474c6009ab3cfa21426f40a5ca7995b8
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-24 13:45:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import discord
-from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Logged in as {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("🏓 Pong!")
+    print(f"✅ Bot is online as {bot.user}")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
