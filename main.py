aa4fc02650dee7ce4d1e0edfcf14bbd317b5fd64
MrMaxNaJa <111119697+NaJaxHub@users.noreply.github.com>
2025-04-24 11:44:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
 import os
 
-intents = discord.Intents.default()
-bot = discord.Bot(intents=intents)
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
